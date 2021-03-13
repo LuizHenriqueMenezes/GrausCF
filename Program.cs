ccf49b1e1df696fa20f20b7d5ae95281dbb70840
Luiz Henrique <lmenezesmessias@gmail.com>
2021-03-13 15:59:37 -0300

    Finalização

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GrausCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vamos converter a temperatura °C para °F, para isso, digite: ");
+
+            Console.WriteLine("Temperatura em °C: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            double multiplicacao = 1.8; 
+            double soma = 32; 
+
+            Console.WriteLine("A conversão do valor pra °F é: " + c * multiplicacao + soma);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # GrausCF
-Converta uma temperatura digitada pelo usuário em °C para °F.  °F = °C × 1,8 + 32  Ref.: https://pt.wikipedia.org/wiki/Celsius
+Converta uma temperatura digitada pelo usuário em °C para °F.  
+°F = °C × 1,8 + 32  
+Ref.: https://pt.wikipedia.org/wiki/Celsius
+
+```cs
+°C = 0
+0°C equivalem a 32°F
+```
+
